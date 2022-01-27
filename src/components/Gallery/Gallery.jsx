e550009b6b542d28fcd2a357b97a5e1a8dd59043
eatoldglue <eatoldglue@MacBook-Pro.local>
2022-01-27 17:31:09 +0100

    added a delete function for the tiny hamster faces

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,59 +1,69 @@
 // import axios from 'axios'
 import { useEffect, useState } from "react";
-import GalleryCard from './GalleryCard'
-import './Gallery.css'
-
-
-
-
+import GalleryCard from "./GalleryCard";
+import "./Gallery.css";
 
 function Gallery() {
+  const [data, setData] = useState();
+  const [reload, setReload] = useState(true);
 
-	const [data, setData] = useState();
+  const deleteHamster = async (hamsterId) => {
+    try {
+      const response = await fetch(`/hamsters/${hamsterId}`, {
+        method: "DELETE",
+      });
+      if (response.status === 200) {
+        console.log("hamster has been eradicated!");
+        setReload(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	useEffect(() => {
-		const allHamsters = async() => {
-			try{
-				const response = await fetch('/hamsters', {method: 'get'})
-				if(response.status === 200) {
-					
-					const data = await response.json()
-					console.log(data[0]);
-					setData(data)
-				} else {
-					throw new Error('Det BRINNER!')
-				}
-
-			} catch(error) {
-				console.log(error.message);
-			}
-		}
-		allHamsters()
-	}, [])
-	return (
-		<div>
-			<div  className='hamsters'>
-				{data && data.map(hamster => {
-					return(
-						<div key={hamster.id}>
-							< GalleryCard hamster={hamster} />
-						</div>
-					)
-				})}
-			</div>
-		</div>
-	)
+  useEffect(() => {
+    const allHamsters = async () => {
+      try {
+        const response = await fetch("/hamsters", { method: "get" });
+        if (response.status === 200) {
+          const data = await response.json();
+          console.log(data[0]);
+          setData(data);
+          setReload(false);
+        } else {
+          setReload(false);
+          throw new Error("Det BRINNER!");
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    if (reload) {
+      allHamsters();
+    }
+  }, [reload]);
+  return (
+    <div>
+      <div className="hamsters">
+        {data &&
+          data.map((hamster) => {
+            return (
+              <div key={hamster.id}>
+                <GalleryCard hamster={hamster} />
+                <button onClick={() => deleteHamster(hamster.id)}>
+                  DELETE
+                </button>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 }
 
-export default Gallery
-
-
-
-
-
+export default Gallery;
 
 // function Gallery() {
-
 
 // 	useEffect(() => {
 // 		async function allHamsters() {
